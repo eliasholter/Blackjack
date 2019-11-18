26e0bfda1ded89d3d9cd55c1aff9b8493f58b0f9
Elias Holter <eliasholter1@gmail.com>
2019-11-18 15:14:44 -0600

    Fixed being able to reroll without using the roll

--- DiceGame.js
+++ DiceGame.js
@@ -2,6 +2,9 @@
 let turnsToPlay = 6;
 let rollValue = 0;
 let operatorType = 0;
+let ableToRoll = true;
+let ableToOperate = false;
+let gameOver = false;
 let goalValue = setComputerStartValue();
 
 document.getElementById("computerScoreDiv").innerHTML = goalValue;
@@ -18,14 +21,18 @@ document.getElementById("userInputInstructions").innerHTML = "Please select your
 // Functions //
 function rollTheDice(diceSides){
 
-    rollValue = Math.floor(Math.random() * (diceSides)) + 1;
+    if(ableToRoll === true && gameOver === false){
+        rollValue = Math.floor(Math.random() * (diceSides)) + 1;
+        ableToRoll = false;
+        ableToOperate = true;
 
-    document.getElementById("currentRollDiv").innerHTML = "";
-    document.getElementById("currentRollDiv").innerHTML = rollValue;
-    document.getElementById("rollsRemainingDiv").innerHTML = "";
-    document.getElementById("rollsRemainingDiv").innerHTML = turnsToPlay - 1;
-    document.getElementById("userInputInstructions").innerHTML = "";
-    document.getElementById("userInputInstructions").innerHTML = "Please select your operator to use on your score from the list in the center:";
+        document.getElementById("currentRollDiv").innerHTML = "";
+        document.getElementById("currentRollDiv").innerHTML = rollValue;
+        document.getElementById("rollsRemainingDiv").innerHTML = "";
+        document.getElementById("rollsRemainingDiv").innerHTML = turnsToPlay - 1;
+        document.getElementById("userInputInstructions").innerHTML = "";
+        document.getElementById("userInputInstructions").innerHTML = "Please select your operator to use on your score from the list in the center:";
+    }
 }
 
 function setComputerStartValue(){
@@ -42,6 +49,7 @@ function setUserStartValue(){
 
 function challengeAFriend(){
     turnsToPlay = 6;
+    gameOver = false;
     userValue = originalUserValue;
     document.getElementById("currentRollDiv").innerHTML = "";    
     document.getElementById("rollsRemainingDiv").innerHTML = "";
@@ -52,44 +60,51 @@ function challengeAFriend(){
 }
 
 function letsPlayARound(userRoll, operatorNumValue){
-    operatorNumValue = parseInt(operatorNumValue);
-    
-    // Update current score based upon user operation selection //
-    if(operatorNumValue === 1){
-        userValue += userRoll;
-    }
-    else if(operatorNumValue === 2){
-        userValue -= userRoll;
-    }
-    else{
-        userValue *= userRoll;
-    }
 
-    let updatedValue = userValue;
-
-    document.getElementById("userScoreDiv").innerHTML = '';
-
-    // Write new score to display area //
-    let userScoreToAdd =  document.createElement("userScoreToAdd");
-    userScoreToAdd.innerHTML = updatedValue;
-    document.getElementById("userScoreDiv").appendChild(userScoreToAdd);
-    document.getElementById("userInputInstructions").innerHTML = "";
-    document.getElementById("userInputInstructions").innerHTML = "Please select your dice to roll from the list on the left:";
-
-
-    turnsToPlay --;
-
-    if(turnsToPlay === 0){
-        // Determine Game Results
-        if(updatedValue < goalValue){
-            let finalScorePercent = ((updatedValue/ goalValue) * 100);
-            document.getElementById("userInputInstructions").innerHTML = "Your score was " + finalScorePercent + "% out of 100%!";
+    if(ableToOperate === true && gameOver === false){
+        operatorNumValue = parseInt(operatorNumValue);
+        
+        // Update current score based upon user operation selection //
+        if(operatorNumValue === 1){
+            userValue += userRoll;
         }
-        else if(updatedValue == goalValue){
-            document.getElementById("userInputInstructions").innerHTML = "You got a perfect score!";
+        else if(operatorNumValue === 2){
+            userValue -= userRoll;
         }
         else{
-            document.getElementById("userInputInstructions").innerHTML = "BUST! Better luck next time!";
+            userValue *= userRoll;
+        }
+
+        let updatedValue = userValue;
+
+        document.getElementById("userScoreDiv").innerHTML = '';
+
+        // Write new score to display area //
+        let userScoreToAdd =  document.createElement("userScoreToAdd");
+        userScoreToAdd.innerHTML = updatedValue;
+        document.getElementById("userScoreDiv").appendChild(userScoreToAdd);
+        document.getElementById("userInputInstructions").innerHTML = "";
+        document.getElementById("userInputInstructions").innerHTML = "Please select your dice to roll from the list on the left:";
+
+        ableToOperate = false;
+        ableToRoll = true;
+        turnsToPlay --;
+
+        if(turnsToPlay === 0){
+            // Determine Game Results
+            if(updatedValue < goalValue){
+                let finalScorePercent = ((updatedValue/ goalValue) * 100);
+                document.getElementById("userInputInstructions").innerHTML = "Your score was " + finalScorePercent + "% out of 100%!";
+                gameOver = true;
+            }
+            else if(updatedValue == goalValue){
+                document.getElementById("userInputInstructions").innerHTML = "You got a perfect score!";
+                gameOver = true;
+            }
+            else{
+                document.getElementById("userInputInstructions").innerHTML = "BUST! Better luck next time!";
+                gameOver = true;
+            }
         }
     }
 }
